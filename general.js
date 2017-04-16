3de2e09e02675183e67c6b567d882f982e09f81a
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-04-16 18:22:30 +0200

    html interactions 101

--- general.js
+++ general.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
  $("#game-play").toggle();
  start();
+ selectShip();
+
 });
 
 
@@ -10,4 +12,29 @@ function start(){
     $("#game-play").toggle();
     $("#enemy-ships").toggle();
   });
+}
+
+function selectShip(check){
+  $(".s").click(function(){
+     console.log(returnShipType($(this)));
+     addShipOnGrid();
+  });
+}
+
+function returnShipType(elem){
+  if(elem.hasClass("aircraft-carrier")) return "aircraft-carrier";
+  if(elem.hasClass("battleship")) return "battleship";
+  if(elem.hasClass("destroyer")) return "destroyer";
+  if(elem.hasClass("cruiser")) return "cruiser";
+  if(elem.hasClass("submarine")) return "submarine";
+}
+
+
+function addShipOnGrid(){
+     console.log("called");
+     $(".col").hover(function(){
+       $(this).addClass("test");
+     },function(){
+       $(this).removeClass("test");
+     });
 }
