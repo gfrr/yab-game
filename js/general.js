30e6062cd476da177b4c47c0efa700e63f3717f2
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-04-17 10:02:48 +0200

    horizontal ship placement done

--- js/general.js
+++ js/general.js
@@ -5,6 +5,16 @@ $(document).ready(function(){
  rotateShip();
 });
 
+//helper function that avoids the manual writing of the game grids
+function appendGrid(target, size) {
+  _.times(size, function(){
+     var row = $("<div class='row'></div>");
+     _.times(size, function(){
+       $(row).append("<div class='col' value='0'></div>");
+     });
+     $(target).append(row);
+  });
+}
 
 function start(){
   $("#start").click(function(){
@@ -17,42 +27,45 @@ function start(){
 
 function selectShip(check){
   $(".s").click(function(){
-     console.log(returnShipType($(this)));
-     addShipOnGrid();
+     addShipOnGrid(returnShipType($(this)), this);
   });
 }
 
 function returnShipType(elem){
-  if(elem.hasClass("aircraft-carrier")) return "aircraft-carrier";
-  if(elem.hasClass("battleship")) return "battleship";
-  if(elem.hasClass("destroyer")) return "destroyer";
-  if(elem.hasClass("cruiser")) return "cruiser";
-  if(elem.hasClass("submarine")) return "submarine";
+  if(elem.hasClass("aircraft-carrier")) return 5;
+  if(elem.hasClass("battleship")) return 4;
+  if(elem.hasClass("cruiser")) return 3;
+  if(elem.hasClass("destroyer")) return 2;
+  if(elem.hasClass("submarine")) return 1;
 }
 
-
-function addShipOnGrid(){
+function addShipOnGrid(size, caller){
      console.log("called");
      $(".col").hover(function(){
-       $(this).addClass("test");
-       $(this).next().addClass("test");
-       var next = $(this).parent().next(); //horizontal targeting
-       var index = $(this).index();
-       var t = $(next).children(".col"); //vertical targeting
-       $(t[index]).addClass("test"); //vertical targeting
-
-       console.log($(".col")[index]);
-       console.log($(this).parent().index(), $(this).index());
-       console.log($(this).next()[0] !== undefined); //checking horizontal boundaries
+       generateShip(this, size);
+      //  $(this).addClass("test");
+      //  $(this).next().addClass("test"); //horizontal targeting
+      //  var next = $(this).parent().next();
+      //  var index = $(this).index();
+      //  var t = $(next).children(".col"); //vertical targeting
+      //  $(t[index]).addClass("test"); //vertical targeting
+       //
+      //  console.log($(".col")[index]);
+      //  console.log($(this).parent().index(), $(this).index());
+      //  console.log($(this).next()[0] !== undefined); //checking horizontal boundaries
        $(this).click(function(){
-         $(this).addClass("final");
+           if($(".test").length == size && $(".test").attr("value") == 0){
+           $(".test").addClass("final");
+           $(".final").attr("value", 1);
+           size = 0;
+           $(caller).toggle();
+         }
        });
      },function(){
-       $(this).removeClass("test");
-       $(this).next().removeClass("test");
+       $(".col").removeClass("test");
      });
-}
 
+}
 
 function rotateShip() {
   $("#rotate").click(function(){
@@ -60,13 +73,15 @@ function rotateShip() {
   });
 }
 
-//helper function that avoids the manual writing of the game grids
-function appendGrid(target, size) {
-  _.times(size, function(){
-     var row = $("<div class='row'></div>");
-     _.times(size, function(){
-       $(row).append("<div class='col'></div>");
-     });
-     $(target).append(row);
-  });
+function generateShip(target, size){
+    var currentTarget = target;
+    if($("#rotate").hasClass("vertical")){
+    } else {
+        for(var i = 0; i < size; i++){
+          if(i > 0){
+            currentTarget = $(currentTarget).next();
+          }
+          $(currentTarget).addClass("test");
+        }
+      }
 }
